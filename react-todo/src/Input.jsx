738ae62325356bda238604758720bbda3fed904c
Xue <261083320@qq.com>
2017-05-31 23:59:19 +0800

    add a input for todo

---
+++ react-todo/src/Input.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class Input extends Component{
+    render(){
+        return(
+            <input type="text" />
+        );
+    }
+}
+
+export default Input;
