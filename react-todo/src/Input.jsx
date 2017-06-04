53c08be2f0f3c87c557ee4efe2a3530c71802a7b
Xue <261083320@qq.com>
2017-06-04 23:18:28 +0800

    listen key event

--- react-todo/src/Input.jsx
+++ react-todo/src/Input.jsx
@@ -2,9 +2,16 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class Input extends Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            tasks : []
+        }
+    }
+    enterKey = ((e) => {if(e.keyCode === 13){e.preventDefault();this.state.tasks.push();}});
     render(){
         return(
-            <input type="text" />
+            <input type="text" onKeyDown={this.enterKey} placeholder="Please enter your task"/>
         );
     }
 }
