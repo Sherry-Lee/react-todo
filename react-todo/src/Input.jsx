1084a56d51e5d3b6d447cf9ac120b33e3c8da20c
Xue <261083320@qq.com>
2017-06-07 23:21:00 +0800

    add keydown push array method

--- react-todo/src/Input.jsx
+++ react-todo/src/Input.jsx
@@ -5,13 +5,24 @@ class Input extends Component{
     constructor(props){
         super(props);
         this.state ={
-            tasks : []
+            tasks : [],
+            key: 1
         }
     }
-    enterKey = ((e) => {if(e.keyCode === 13){e.preventDefault();this.state.tasks.push();}});
+    enterKey = ((e) => {
+        if(e.keyCode === 13){
+        e.preventDefault();
+        this.setState({tasks: this.state.tasks.concat(<li key={this.state.key}>{e.target.value}</li>)});
+        this.setState({key: this.state.key + 1});
+}});
     render(){
         return(
-            <input type="text" onKeyDown={this.enterKey} placeholder="Please enter your task"/>
+            <div className="todo">
+                <input type="text" id="task" onKeyDown={this.enterKey} placeholder="Please enter your task"/>
+                <ul>
+                    {this.state.tasks}
+                </ul>
+            </div>
         );
     }
 }
